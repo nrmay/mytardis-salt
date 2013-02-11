cae71002b65d3fd23240fc9ac08081fb4b74b56c
Grischa Meyer <grischa@gmail.com>
2013-02-11 15:42:33 +1100

    runs automated tests with vagrant

---
+++ test/README.rst
@@ -0,0 +1,4 @@
+Testing with vagrant
+====================
+
+Test the salt state tree on vagrant by simply running ``python test.py``
---
+++ test/test.py
@@ -0,0 +1,32 @@
+
+logfilename = "test.log"
+
+import subprocess
+import urllib
+import os
+
+logfile = open(logfilename, 'w')
+
+cwd = os.path.dirname(os.path.abspath(__file__))
+salt_dir = os.path.dirname(cwd)
+
+testcases = ["vagrant-centos",
+             "vagrant-ubuntu"]
+# print salt_dir
+# print cwd
+# quit()
+for case in testcases:
+    print "Testing case %s" % case
+    logfile.write("Test Case: %s\n\n" % case)
+    subprocess.call("vagrant up", cwd=os.path.join(salt_dir, case),
+                    stdout=logfile, stderr=subprocess.STDOUT, shell=True)
+    urllib.urlretrieve("http://localhost:8000", "%s-index.html" % case)
+    subprocess.call("vagrant destroy -f", cwd=os.path.join(salt_dir, case),
+                    stdout=logfile, stderr=subprocess.STDOUT, shell=True)
+    print "Test result"
+    if subprocess.call("diff index.html %s-index.html" % case, shell=True) == 0:
+        print "Test successful"
+    else:
+        print "Check logfiles for errors"
+
+logfile.close()
